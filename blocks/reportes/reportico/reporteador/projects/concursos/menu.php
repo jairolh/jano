93e65700889e25990cb85e0a3f7c02c5411c45fc
Jairo Lavado Hernandez <jlavadoh@correo.udistrital.edu.co>
2019-05-27 19:58:46 -0500

    Desarrollo reporte resumen verificación de requisitos

--- blocks/reportes/reportico/reporteador/projects/concursos/menu.php
+++ blocks/reportes/reportico/reporteador/projects/concursos/menu.php
@@ -1,6 +1,22 @@
 <?php
+/*
 $menu_title = SW_PROJECT_TITLE;
 $menu = array (
 	array ( "language" => "en_gb", "report" => ".*\.xml", "title" => "<AUTO>" )
-	);
+	);*/
+
+$menu_title = 'Reportes';
+$menu = array();
+
+$dropdown_menu = array(
+        		array(
+				"project" => "concursos",
+				"title" => "Concursos",
+				"items" => array(
+						array("reportfile" => "listado_evaluadores.xml", "title" => "Listado usuarios evaluadores"),
+						array("reportfile" => "verifico_requisitos.xml", "title" => "Resumen verificación requisitos"),
+				)
+		),
+		 
+);
 ?>
